e7b43956224ef6f70bbd7e080367f99916c325c1
tonmoy-free <tonmoykhan.free@gmail.com>
2026-02-15 22:51:56 +0600

    better auth add

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
 
-export default function Home() {
+export default async function Home() {
+  const session = await authClient.getSession();
+
+  console.log(session)
   return (
     <div>
       <Button variant="outline">Click Here</Button>
--- src/app/(commonLayout)/register/page.tsx
+++ src/app/(commonLayout)/register/page.tsx
@@ -1,4 +1,4 @@
-import { RegisterForm } from "@/components/register-form"
+import { RegisterForm } from "@/components/modules/authentication/register-form"
 
 export default function Page() {
   return (
